c6878f8f84326b9cd1d29edde5c18ec08dcd6c4a
Selebogo <scmosoeu@gmail.com>
2019-11-28 05:40:43 +0200

    Initial website finished

---
+++ dist/js/work.js
@@ -0,0 +1,9 @@
+$(() => {
+    const $gallery = $('.item a').simpleLightbox(
+        {
+            alertError:false,
+            animationSpeed:10,
+            showCounter:false
+        }
+    );
+})
